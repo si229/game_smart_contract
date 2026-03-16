34903ecf03df546ea17d31ccf4b9a07c4f5bdc7b
ss <nm_jok@TAICHISGN1.LOCAL>
2026-03-16 17:16:15 +0800

    a

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,19 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("hardhat-deploy");
+
+
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 
 module.exports = {
   solidity: {
@@ -6,8 +21,28 @@ module.exports = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
-      },
-    },
+        runs: 200
+      }
+    }
   },
+  networks: {
+    hardhat: {
+      chainId: 31337
+    },
+    // Sepolia 测试网络配置
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "YOUR_SEPOLIA_RPC_URL", // 填写你从提供商获得的 RPC URL
+      accounts: process.env.SEPOLIA_PRIVATE_KEY ? [process.env.SEPOLIA_PRIVATE_KEY] : [],
+      chainId: 11155111
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
 };
